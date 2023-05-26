35943350c80132e882e622cfce9d3b6dc1471e3e
Dominduchami <dominikkobinski314@gmail.com>
2023-05-27 00:29:17 +0200

    Project: Add NBH creation tools from cLK
    Add ngben and yang tools from cedesmith's lk.
    Upstream url: https://github.com/cedesmith/cLK
    
    Co-authored-by: cedesmith <cedesmith@gmail.com>

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ GCC5_ARM_PREFIX=arm-linux-gnueabihf- build -s -n 0 -a ARM -t GCC5 -p HtcLeoPkg/H
 chmod +x build_boot_shim.sh
 ./build_boot_shim.sh
 
-cat BootShim/BootShim.bin workspace/Build/QSD8250/DEBUG_GCC5/FV/QSD8250_UEFI.fd >>ImageResources/bootpayload.bin
+cat BootShim/BootShim.bin workspace/Build/QSD8250/DEBUG_GCC5/FV/QSD8250_UEFI.fd >>ImageResources/lk.bin
 
-mkbootimg --kernel ImageResources/bootpayload.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/uefi.img
+mkbootimg --kernel ImageResources/lk.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/uefi.img
+
+cd ImageResources
+./nbgen os.nb
+./yang -F LEOIMG.nbh -f os.nb -t 0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
+cd ../
