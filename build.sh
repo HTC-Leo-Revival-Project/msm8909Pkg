a530c18fe236395e392e0e785d34dce598ba8cc4
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-02-21 20:13:03 +0100

    ImageResources: Use prebuilt yang for now

--- build.sh
+++ build.sh
@@ -18,13 +18,14 @@ mkbootimg --kernel ImageResources/Tools/bootpayload.bin --base 0x11800000 --kern
 if [ ! -f ImageResources/Tools/nbgen ]; then
 	gcc -std=c99 ImageResources/Tools/nbgen.c -o ImageResources/Tools/nbgen
 fi
+# We're using a prebuild yang binary for now, since the compiled version doesn't seem to produce a valid NBH for Leo
 if [ ! -f ImageResources/Tools/yang ]; then
-	gcc  ImageResources/Tools/yang/nbh.c ImageResources/Tools/yang/nbhextract.c ImageResources/Tools/yang/yang.c -o ImageResources/Tools/yangbin
+	gcc ImageResources/Tools/yang/nbh.c ImageResources/Tools/yang/nbhextract.c ImageResources/Tools/yang/yang.c -o ImageResources/Tools/yangbin
 fi
 
 cd ImageResources/Tools
 ./nbgen os.nb
-./yangbin -F ../LEOIMG.nbh -f logo.nb,os.nb -t 0x600,0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
+./yang -F ../LEOIMG.nbh -f logo.nb,os.nb -t 0x600,0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
 rm *.bin
 rm os.nb
 cd ../../
