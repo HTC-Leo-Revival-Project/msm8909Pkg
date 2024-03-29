4dde8009e7ff93b12ac3626afac709a84f6f0fbd
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-03-29 15:03:49 +0100

    Experiment

--- HtcLeoPkg/Library/HtcLeoPkgLib/HtcLeoPkgMem.c
+++ HtcLeoPkg/Library/HtcLeoPkgLib/HtcLeoPkgMem.c
@@ -1,6 +1,7 @@
 /** @file
 *
 *  Copyright (c) 2011, ARM Limited. All rights reserved.
+*  Copyright (c) 2019, RUIKAI LIU and MR TUNNEL. All rights reserved.
 *
 *  This program and the accompanying materials
 *  are licensed and made available under the terms and conditions of the BSD License
@@ -14,18 +15,8 @@
 
 #include <Library/ArmPlatformLib.h>
 #include <Library/DebugLib.h>
-#include <Library/IoLib.h>
-#include <Library/MemoryAllocationLib.h>
-#include <Library/PcdLib.h>
-
-#define MAX_VIRTUAL_MEMORY_MAP_DESCRIPTORS  16
-
-// DDR attributes
-#define DDR_ATTRIBUTES_CACHED               ARM_MEMORY_REGION_ATTRIBUTE_WRITE_BACK
-#define DDR_ATTRIBUTES_UNCACHED             ARM_MEMORY_REGION_ATTRIBUTE_UNCACHED_UNBUFFERED
-
-#define SOC_REGISTERS_ATTRIBUTES            ARM_MEMORY_REGION_ATTRIBUTE_DEVICE
-
+#include <Library/HobLib.h>
+#include <Configuration/DeviceMemoryMap.h>
 /**
   Return the Virtual Memory Map of your platform
 
@@ -36,72 +27,67 @@
                                     entry
 
 **/
+
+STATIC
+VOID
+AddHob
+(
+    PARM_MEMORY_REGION_DESCRIPTOR_EX Desc
+)
+{
+	BuildResourceDescriptorHob(
+		Desc->ResourceType,
+		Desc->ResourceAttribute,
+		Desc->Address,
+		Desc->Length
+	);
+
+	BuildMemoryAllocationHob(
+		Desc->Address,
+		Desc->Length,
+		Desc->MemoryType
+	);
+}
+
 VOID
 ArmPlatformGetVirtualMemoryMap (
   IN ARM_MEMORY_REGION_DESCRIPTOR** VirtualMemoryMap
   )
 {
-  // You are not expected to call this
-	ASSERT(FALSE);
-  
-  ARM_MEMORY_REGION_ATTRIBUTES  CacheAttributes;
-  UINTN                         Index = 0;
-  ARM_MEMORY_REGION_DESCRIPTOR  *VirtualMemoryTable;
+  //TO-DO:ADD MEMORY MAP HERE
+    PARM_MEMORY_REGION_DESCRIPTOR_EX MemoryDescriptorEx = gDeviceMemoryDescriptorEx;
+    ARM_MEMORY_REGION_DESCRIPTOR MemoryDescriptor[MAX_ARM_MEMORY_REGION_DESCRIPTOR_COUNT];
+    UINTN Index = 0;
 
-  ASSERT (VirtualMemoryMap != NULL);
-  DEBUG ((DEBUG_VERBOSE, "Enter: ArmPlatformGetVirtualMemoryMap\n"));
-  VirtualMemoryTable = (ARM_MEMORY_REGION_DESCRIPTOR *) AllocatePages (
-      EFI_SIZE_TO_PAGES (sizeof (ARM_MEMORY_REGION_DESCRIPTOR) *
-      MAX_VIRTUAL_MEMORY_MAP_DESCRIPTORS));
-  if (VirtualMemoryTable == NULL) {
-    return;
-  }
+    // Run through each memory descriptor
+    while (MemoryDescriptorEx->Address != (EFI_PHYSICAL_ADDRESS)0xFFFFFFFF)
+    {
+        switch (MemoryDescriptorEx->HobOption)
+        {
+            case AddMem:
+			case AddDev:
+                AddHob(MemoryDescriptorEx);
+                break;
+            case NoHob:
+            default:
+                goto update;
+        }
 
-  CacheAttributes = DDR_ATTRIBUTES_CACHED;
+    update:
+        ASSERT(Index < MAX_ARM_MEMORY_REGION_DESCRIPTOR_COUNT);
 
-  // SOC registers region
-  VirtualMemoryTable[Index].PhysicalBase   = 0x00000000;
-  VirtualMemoryTable[Index].VirtualBase    = 0x00000000;
-  VirtualMemoryTable[Index].Length         = 0x11800000;
-  VirtualMemoryTable[Index].Attributes     = SOC_REGISTERS_ATTRIBUTES;
+        MemoryDescriptor[Index].PhysicalBase = MemoryDescriptorEx->Address;
+        MemoryDescriptor[Index].VirtualBase = MemoryDescriptorEx->Address;
+        MemoryDescriptor[Index].Length = MemoryDescriptorEx->Length;
+		MemoryDescriptor[Index].Attributes = MemoryDescriptorEx->ArmAttributes;
 
-  // Rearranged system memory regions
-  VirtualMemoryTable[++Index].PhysicalBase = 0x11800000;
-  VirtualMemoryTable[Index].VirtualBase    = 0x11800000;
-  VirtualMemoryTable[Index].Length         = 0x00008000;
-  VirtualMemoryTable[Index].Attributes     = ARM_MEMORY_REGION_ATTRIBUTE_WRITE_THROUGH;
+        Index++;
+        MemoryDescriptorEx++;
+    }
 
-  // FD
-  VirtualMemoryTable[++Index].PhysicalBase = 0x11808000;
-  VirtualMemoryTable[Index].VirtualBase    = 0x11808000;
-  VirtualMemoryTable[Index].Length         = 0x00200000;
-  VirtualMemoryTable[Index].Attributes     = CacheAttributes;
-
-  // Free memory
-  VirtualMemoryTable[++Index].PhysicalBase = 0x11A08000;
-  VirtualMemoryTable[Index].VirtualBase    = 0x11A08000;
-  VirtualMemoryTable[Index].Length         = 0x1E5F8000;
-  VirtualMemoryTable[Index].Attributes     = CacheAttributes;
-
-  // VIC region
-  VirtualMemoryTable[++Index].PhysicalBase = 0xAC000000;
-  VirtualMemoryTable[Index].VirtualBase    = 0xAC000000;
-  VirtualMemoryTable[Index].Length         = 0x00100000;
-  VirtualMemoryTable[Index].Attributes     = SOC_REGISTERS_ATTRIBUTES;
-
-  // GPT region
-  VirtualMemoryTable[++Index].PhysicalBase = 0xAC100000;
-  VirtualMemoryTable[Index].VirtualBase    = 0xAC100000;
-  VirtualMemoryTable[Index].Length         = 0x00100000;
-  VirtualMemoryTable[Index].Attributes     = SOC_REGISTERS_ATTRIBUTES;
-
-  // End of Table
-  VirtualMemoryTable[++Index].PhysicalBase = 0;
-  VirtualMemoryTable[Index].VirtualBase    = 0;
-  VirtualMemoryTable[Index].Length         = 0;
-  VirtualMemoryTable[Index].Attributes     = (ARM_MEMORY_REGION_ATTRIBUTES)0;
-
-  ASSERT ((Index + 1) <= MAX_VIRTUAL_MEMORY_MAP_DESCRIPTORS);
-
-  *VirtualMemoryMap = VirtualMemoryTable;
+    // Last one (terminator)
+    ASSERT(Index < MAX_ARM_MEMORY_REGION_DESCRIPTOR_COUNT);
+    
+    *VirtualMemoryMap = MemoryDescriptor;
+  //ASSERT(0);
 }
