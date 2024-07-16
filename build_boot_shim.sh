44c05a1e6b93ca62cb3979e5efa77a0043c24f7d
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-07-16 18:19:36 +0200

    BootShim: Drop per-device configs

--- build_boot_shim.sh
+++ build_boot_shim.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-if [ $1 = "Passion" ] || [ $1 = "Bravo" ] || [ $1 = "Leo" ]; then
-    cd BootShim
-    make UEFI_BASE=0x2C000000 UEFI_SIZE=0x00100000
-    rm BootShim.elf
-    cd ..
-elif [ $1 = "Schubert" ]; then
-    cd BootShim
-    make UEFI_BASE=0x2C000000 UEFI_SIZE=0x00100000
-    rm BootShim.elf
-    cd ..
-else
-    echo "BootShim: Invalid platform"
-fi
+cd BootShim
+make UEFI_BASE=0x2C000000 UEFI_SIZE=0x00100000
+rm BootShim.elf
+cd ..
