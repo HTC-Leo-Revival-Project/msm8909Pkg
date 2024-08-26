dea5e7675cfa4de37a3080293a28d0173788113f
Fehér Roland Ádám <feherneoh@gmail.com>
2024-08-26 10:29:01 +0200

    build_boot_images: use case instead of chained ifs

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-if [ $1 == 'Leo' ]; then
-    cat BootShim/BootShim.bin $WORKSPACE/Build/HtcLeo/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/Leo/bootpayload.bin
 
-    mkbootimg --kernel ImageResources/Leo/bootpayload.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/Leo/uefi.img
+case $1 in
+    Leo)
+        cat BootShim/BootShim.bin $WORKSPACE/Build/HtcLeo/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/Leo/bootpayload.bin
 
-    # NBH creation
-    if [ ! -f ImageResources/Leo/nbgen ]; then
-        gcc -std=c99 ImageResources/Leo/nbgen.c -o ImageResources/Leo/nbgen
-    fi
-    # We're using a prebuild yang binary for now, since the compiled version doesn't seem to produce a valid NBH for Leo
-    if [ ! -f ImageResources/Leo/yang ]; then
-        gcc ImageResources/Leo/yang_src/nbh.c ImageResources/Leo/yang_src/nbhextract.c ImageResources/Leo/yang_src/yang.c -o ImageResources/Leo/yangbin
-    fi
+        mkbootimg --kernel ImageResources/Leo/bootpayload.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/Leo/uefi.img
 
-    cd ImageResources/Leo/
-    ./nbgen os.nb
-    ./yang -F LEOIMG.nbh -f logo.nb,os.nb -t 0x600,0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
-    cd ../../
-elif [ $1 == 'Schubert' ]; then
-    cat BootShim/BootShim.bin $WORKSPACE/Build/HtcSchubert/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/$1/bootpayload.bin
+        # NBH creation
+        if [ ! -f ImageResources/Leo/nbgen ]; then
+            gcc -std=c99 ImageResources/Leo/nbgen.c -o ImageResources/Leo/nbgen
+        fi
+        # We're using a prebuild yang binary for now, since the compiled version doesn't seem to produce a valid NBH for Leo
+        if [ ! -f ImageResources/Leo/yang ]; then
+            gcc ImageResources/Leo/yang_src/nbh.c ImageResources/Leo/yang_src/nbhextract.c ImageResources/Leo/yang_src/yang.c -o ImageResources/Leo/yangbin
+        fi
 
-    mkbootimg --kernel ImageResources/$1/bootpayload.bin --base 0x20000000 --kernel_offset 0x00008000 -o ImageResources/$1/uefi.img
-elif [ $1 == 'Gold' ]; then
-    cat BootShim/BootShim.bin $WORKSPACE/Build/HtcGold/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/$1/bootpayload.bin
+        cd ImageResources/Leo/
+        ./nbgen os.nb
+        ./yang -F LEOIMG.nbh -f logo.nb,os.nb -t 0x600,0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
+        cd ../../
+        ;;
+    Schubert)
+        cat BootShim/BootShim.bin $WORKSPACE/Build/HtcSchubert/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/$1/bootpayload.bin
 
-    mkbootimg --kernel ImageResources/$1/bootpayload.bin --base 0x20000000 --kernel_offset 0x00008000 -o ImageResources/$1/uefi.img
-else
-    echo "Bootimages: Invalid platform ($1)"
-    exit 1
-fi
+        mkbootimg --kernel ImageResources/$1/bootpayload.bin --base 0x20000000 --kernel_offset 0x00008000 -o ImageResources/$1/uefi.img
+        ;;
+    Gold)
+        cat BootShim/BootShim.bin $WORKSPACE/Build/HtcGold/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/$1/bootpayload.bin
+
+        mkbootimg --kernel ImageResources/$1/bootpayload.bin --base 0x20000000 --kernel_offset 0x00008000 -o ImageResources/$1/uefi.img
+        ;;
+    *)
+        echo "Bootimages: Invalid platform ($1)"
+        exit 1
+        ;;
+esac
 exit 0
