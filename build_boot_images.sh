d11531f830078467806eb44f9be51e5dc22eeb4e
Fehér Roland Ádám <feherneoh@gmail.com>
2024-08-26 11:08:13 +0200

    Tools: Move to separate directory
    Made Makefile for and moved:
    - `nbgen`
    - `yang`

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -7,18 +7,13 @@ case $1 in
         mkbootimg --kernel ImageResources/Leo/bootpayload.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/Leo/uefi.img
 
         # NBH creation
-        if [ ! -f ImageResources/Leo/nbgen ]; then
-            gcc -std=c99 ImageResources/Leo/nbgen.c -o ImageResources/Leo/nbgen
-        fi
-        # We're using a prebuild yang binary for now, since the compiled version doesn't seem to produce a valid NBH for Leo
-        if [ ! -f ImageResources/Leo/yang ]; then
-            gcc ImageResources/Leo/yang_src/nbh.c ImageResources/Leo/yang_src/nbhextract.c ImageResources/Leo/yang_src/yang.c -o ImageResources/Leo/yangbin
-        fi
+        make -C Tools/nbgen clean all
+        make -C Tools/yang clean all
 
         cd ImageResources/Leo/
-        ./nbgen os.nb
-        ./yang -F LEOIMG.nbh -f logo.nb,os.nb -t 0x600,0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
+        ../../Tools/nbgen/nbgen os.nb
         cd ../../
+        Tools/yang/yang -F ImageResources/Leo/LEOIMG.nbh -f ImageResources/Leo/logo.nb,ImageResources/Leo/os.nb -t 0x600,0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
         ;;
     Schubert)
         cat BootShim/BootShim.bin $WORKSPACE/Build/HtcSchubert/DEBUG_GCC/FV/QSD8250_UEFI.fd > ImageResources/$1/bootpayload.bin
